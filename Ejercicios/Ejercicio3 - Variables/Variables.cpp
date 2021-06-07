9c932c3990209999db5736d047f392e2fe19e764
Elvin Palma <enpalma@unah.hn>
2021-06-07 01:24:23 -0600

    Agregar Ejercicio 3 y 4

---
+++ Ejercicios/Ejercicio3 - Variables/Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int NumeroEntero;
+    NumeroEntero = 15;
+
+    cout << "Entero: " << NumeroEntero << endl;
+
+    NumeroEntero = 18;
+
+    cout << "Entero Modificado: " << NumeroEntero << endl;
+
+
+    return 0;
+}
