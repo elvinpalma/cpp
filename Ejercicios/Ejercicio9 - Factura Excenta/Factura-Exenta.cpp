9b9e15542aa18bb4bf824473332d28323eec7fa3
Elvin Palma <enpalma@unah.hn>
2021-06-27 13:00:01 -0600

    Agregar Ejercicio9-Factura-Exenta

---
+++ Ejercicios/Ejercicio9 - Factura Excenta/Factura-Exenta.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+	//Datos de entrada
+    double Subtotal = 0;
+    double Total = 0;
+    double Impuesto = 0.15;
+    int Descuento = 0;
+    double CalculoDescuento = 0;
+    double CalculoImpuesto = 0;
+
+    cout << "Ingrese el subtotal de la factura: ";
+    cin >> Subtotal;
+    
+    cout << endl;
+
+    cout << "Ingrese el descuento (0, 10, 15, 20): ";
+    cin >> Descuento;
+    cout << endl;
+
+    //Proceso
+    CalculoDescuento = (Subtotal * Descuento) / 100;
+
+        cout << endl;
+    char Exenta = ' ';
+    cout << "Factura exenta?";
+    cout << endl;
+    cout << "Encriba s / n: ";
+    cin >> Exenta;
+
+    if (Exenta == 's' || Exenta == 'S')
+    {
+        CalculoImpuesto = 0;
+    }
+    else
+        if (Exenta == 'n' || Exenta == 'N')
+        {
+            CalculoImpuesto = (Subtotal - CalculoDescuento) * Impuesto;
+        }
+
+    Total = Subtotal - CalculoDescuento + CalculoImpuesto;
+
+
+    
+
+    //Salida
+    cout << endl;
+    cout << "Total a pagar es: " << Total;
+    cout << endl;
+    
+	return 0;
+}
